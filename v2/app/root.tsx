65dca94b231448f2dbae3a6e6172378abb50f141
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-05-09 12:08:25 +0200

    initial commit with tailwind

---
+++ v2/app/root.tsx
@@ -0,0 +1,36 @@
+import type { LinksFunction } from "@remix-run/cloudflare";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => {
+  const appLinks = [{ rel: "stylesheet", href: styles }];
+  cssBundleHref && appLinks.push({ rel: "stylesheet", href: cssBundleHref });
+  return appLinks;
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
